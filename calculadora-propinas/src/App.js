99dd8c2ff7517ee08f2b9062fa793361d6523e62
Eduardhaim <Eduardogustavotur@gmail.com>
2025-08-10 09:15:49 -0600

    Reapply "cambios"
    This reverts commit 7388bd18c0c599bec71c3a44f6745259fa6d51af.

--- calculadora-propinas/src/App.js
+++ calculadora-propinas/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import TipCalculator from "./components/TipCalculator";
+import MiCalculadoraPropinas from "./components/MiCalculadoraPropinas";
 
 function App() {
   return (
-    <div>
-      <TipCalculator />
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Calculadora de Propinas 💵</h1>
+      <MiCalculadoraPropinas />
     </div>
   );
 }
